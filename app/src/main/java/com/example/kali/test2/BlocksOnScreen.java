cc8efed3a21c19c04a9000076240c4f53c0711c9
root <patiltejas24@gmail.com>
2016-09-09 10:36:26 -0400

    added all colors n acitivity to create blocks of screen

--- app/src/main/java/com/example/kali/test2/BlocksOnScreen.java
+++ app/src/main/java/com/example/kali/test2/BlocksOnScreen.java
@@ -2,15 +2,12 @@ package com.example.kali.test2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.RelativeLayout;
 
-
-public class MainActivity extends AppCompatActivity {
+public class BlocksOnScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.blocks_on_screen);
     }
 }
